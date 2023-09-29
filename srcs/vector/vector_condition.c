2eb5838aa44031b212c8cd08132085edf1880aac
Beomjin Na <bena@c10r8s1.42seoul.kr>
2023-09-29 13:52:59 +0900

    FIX: Fix zero vector condition

--- srcs/vector/vector_condition.c
+++ srcs/vector/vector_condition.c
@@ -6,7 +6,7 @@
 /*   By: bena <bena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/14 16:52:35 by bena              #+#    #+#             */
-/*   Updated: 2023/09/28 18:07:53 by bena             ###   ########.fr       */
+/*   Updated: 2023/09/29 13:52:09 by bena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ int	is_vector_zero(t_vector vec)
 {
 	if (vec == NULL)
 		return (-1);
-	if (vec[0] < M_VECTOR_MIN_SCALE
-		&& vec[1] < M_VECTOR_MIN_SCALE
-		&& vec[2] < M_VECTOR_MIN_SCALE)
+	if (-M_VECTOR_MIN_SCALE < vec[0] && vec[0] < M_VECTOR_MIN_SCALE
+	&& -M_VECTOR_MIN_SCALE < vec[1] && vec[1] < M_VECTOR_MIN_SCALE
+	&& -M_VECTOR_MIN_SCALE < vec[2] && vec[2] < M_VECTOR_MIN_SCALE)
 		return (1);
 	return (0);
 }
