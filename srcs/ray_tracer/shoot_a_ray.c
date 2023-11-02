a17f4807ffb9c113cd345a311235d891b26018a4
Beomjin Na <bena@c2r5s4.42seoul.kr>
2023-11-02 18:36:22 +0900

    FEAT: Add reflection optimization

--- srcs/ray_tracer/shoot_a_ray.c
+++ srcs/ray_tracer/shoot_a_ray.c
@@ -6,7 +6,7 @@
 /*   By: bena <bena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 16:51:28 by bena              #+#    #+#             */
-/*   Updated: 2023/11/02 18:29:02 by bena             ###   ########.fr       */
+/*   Updated: 2023/11/02 18:35:50 by bena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,14 +37,17 @@ static void	get_value_from_hitpoint(t_vector buffer,
 	t_ray		temp_ray;
 	t_vector	temp_output;
 
-	temp_ray = set_ray(hitpoint->position,
-			hitpoint->reflection_direction_unit, hitpoint->reflection_ratio);
-	shoot_a_ray(temp_output, temp_ray, data, depth + 1);
-	vec_add(buffer, buffer, temp_output);
-	compute_lighting_from_spotlights(temp_output, hitpoint, data);
-	vec_add(buffer, buffer, temp_output);
+	if (hitpoint->reflection_ratio > M_VECTOR_MIN_SCALE)
+	{
+		temp_ray = set_ray(hitpoint->position,
+				hitpoint->reflection_direction_unit, hitpoint->reflection_ratio);
+		shoot_a_ray(temp_output, temp_ray, data, depth + 1);
+		vec_add(buffer, buffer, temp_output);
+	}
 	if (hitpoint->reflection_ratio < 1.0 - M_VECTOR_MIN_SCALE)
 	{
+		compute_lighting_from_spotlights(temp_output, hitpoint, data);
+		vec_add(buffer, buffer, temp_output);
 		compute_scattering_lights(temp_output, hitpoint, data, depth + 1);
 		vec_add(buffer, buffer, temp_output);
 	}
