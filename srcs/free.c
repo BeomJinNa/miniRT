9e4a203d525bf7e4fe876c41988aa41d3702273d
Beomjin Na <bena@c2r5s4.42seoul.kr>
2023-11-02 19:45:04 +0900

    FIX: Edit free to free_safe

--- srcs/free.c
+++ srcs/free.c
@@ -6,7 +6,7 @@
 /*   By: bena <bena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 17:14:12 by bena              #+#    #+#             */
-/*   Updated: 2023/11/02 19:37:02 by bena             ###   ########.fr       */
+/*   Updated: 2023/11/02 19:43:52 by bena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,27 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+static void	free_safe(void *address);
+
 void	free_malloced_memories(t_stat *stat)
 {
 	if (stat == NULL)
 		return ;
 	remove_tree(&stat->data.tree);
 	if (stat->data.objects != NULL)
-		ft_lstclear(&stat->data.objects, free);
+		ft_lstclear(&stat->data.objects, free_safe);
 	if (stat->data.lights != NULL)
-		ft_lstclear(&stat->data.lights, free);
+		ft_lstclear(&stat->data.lights, free_safe);
 	if (stat->data.images != NULL)
-		ft_lstclear(&stat->data.images, free);
+		ft_lstclear(&stat->data.images, free_safe);
 	if (stat->data.cam.image.data != NULL)
 		free(stat->data.cam.image.data);
+}
+
+static void	free_safe(void *address)
+{
+	if (address != NULL)
+		free(address);
 }
 
 void	print_error(char *str)
